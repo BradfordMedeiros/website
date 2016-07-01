a74fd6e4729aa11e8cf179e1b73612a707ddc430
bradmedeiros <bradmedeiros0@gmail.com>
2016-07-01 19:10:09 -0400

    -

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var location_services = new (require('./location_services.js'))();
+var port = 80;
 
 
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
@@ -21,12 +14,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/location',location_services.get_router(express));
 app.get('/', function(req,res, next){
-  res.sendFile('views/main.html',{root: path.join(__dirname)});
+  console.log('got request');
+  res.sendFile('public/main.html',{root: path.join(__dirname)});
 });
 
 
-app.listen(3000, function(){
-
+app.listen(port, function(){
+    console.log('listening on port: '+port);
 });
 
 module.exports = app;
